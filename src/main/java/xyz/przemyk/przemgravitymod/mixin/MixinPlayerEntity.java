d75ce2fd0dd460edc9cd7619f7f74804a475c9f4
Przemyk <przemek@przemyk.xyz>
2021-05-07 21:55:42 +0200

    fixed jumping

--- src/main/java/xyz/przemyk/przemgravitymod/mixin/MixinPlayerEntity.java
+++ src/main/java/xyz/przemyk/przemgravitymod/mixin/MixinPlayerEntity.java
@@ -5,6 +5,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity extends LivingEntity {
@@ -37,6 +40,42 @@ public abstract class MixinPlayerEntity extends LivingEntity {
     public float getStandingEyeHeight(Pose pose, EntitySize entitySize) {
         return entitySize.height * 0.15F;
     }
+
+    @Inject(method = "jumpFromGround", at = @At("HEAD"))
+    public void adjustYRotBeforeJump(CallbackInfo ci) {
+        yRot += 180f;
+    }
+
+    @Inject(method = "jumpFromGround", at = @At("RETURN"))
+    public void adjustYRotAfterJump(CallbackInfo ci) {
+        yRot -= 180f;
+    }
+
+//    @Override ????
+//    protected float tickHeadTurn(float p_110146_1_, float movedLastTickLength) {
+//        float f = MathHelper.wrapDegrees(p_110146_1_ - this.yBodyRot);
+//        this.yBodyRot += f * 0.3F;
+//        float f1 = MathHelper.wrapDegrees(this.yRot - this.yBodyRot);
+//        boolean flag = f1 < -90.0F || f1 >= 90.0F;
+//        if (f1 < -75.0F) {
+//            f1 = -75.0F;
+//        }
+//
+//        if (f1 >= 75.0F) {
+//            f1 = 75.0F;
+//        }
+//
+//        this.yBodyRot = this.yRot - f1;
+//        if (f1 * f1 > 2500.0F) {
+//            this.yBodyRot += f1 * 0.2F;
+//        }
+//
+//        if (flag) {
+//            movedLastTickLength *= -1.0F;
+//        }
+//
+//        return movedLastTickLength;
+//    }
 }
 
 
